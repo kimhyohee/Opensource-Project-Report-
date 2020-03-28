6e52aabdf090c7e85347b73289538abd42e8bcab
kimhyohee <49019232+kimhyohee@users.noreply.github.com>
2020-03-28 22:36:32 +0900

    Add files via upload

---
+++ [오픈소스전문프로젝트 Report #01]오지현, 김효희, 이제은, 김은진, 김소현/[오픈소스전문프로젝트 Report #01]4조_3번 과제 코드/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.fourbutton;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    Button button1;
+    Button button2;
+    Button button3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://m.nate.com"));
+            startActivity(intent);
+            }
+        });
+
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:"));
+                startActivity(myIntent); }
+        });
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivity(intent);
+            }
+        });
+
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
